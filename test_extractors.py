dd7e522bdddcb42f64b3b7b3cfa012523c55d389
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2019-11-20 14:43:09 +0100

    included test cases for the extractors

---
+++ test_extractors.py
@@ -0,0 +1,9 @@
+from src.extractors import extract_city, extract_neighbors
+
+def test_extract_city():
+    assert extract_city('Foo north=Bar west=Baz south=Qu-ux') == 'Foo'
+    assert extract_city('Bar south=Foo west=Bee') == 'Bar'
+
+def test_extract_neighbors():
+    assert extract_neighbors('Foo north=Bar west=Baz south=Qu-ux') == ['Bar','Baz','Qu-ux'] 
+    assert extract_neighbors('Bar south=Foo west=Bee') == ['Foo','Bee'] 
