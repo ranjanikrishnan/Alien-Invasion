1949966fdc7c6516464cff2b160037e7b8e31378
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2019-11-24 14:46:48 +0100

    Removed and modified module structure

--- src/extractors.py
+++
@@ -1,8 +0,0 @@
-def extract_city(city_connections):
-    return city_connections.split(' ')[0]
-
-def extract_neighbors(city_connections):
-    neighbors =[]
-    separated = city_connections.split(' ')[1:]
-    neighbors = [each.lstrip("north=south=west=").rstrip('\n') for each in separated]
-    return neighbors
